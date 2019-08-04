4229b8f2f8383600299cd6be258414df89e62bc6
zoton2 <zoton2@gmail.com>
2019-08-04 03:45:32 +0100

    Some linting/tsconfig tweaks

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,4 +1,4 @@
-import { NodeCG } from 'nodecg/types/server';
+import { NodeCG } from 'nodecg/types/server'; // eslint-disable-line
 
 export = (nodecg: NodeCG): void => {
   nodecg.log.info('Extension code working!');
