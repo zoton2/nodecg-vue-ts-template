734b8651d167ebfc349d39d7f678935e01dd646e
zoton2 <zoton2@gmail.com>
2019-09-03 12:03:13 +0100

    Move NodeCG browser helper file

--- src/browser_shared/nodecg.ts
+++ src/browser_shared/nodecg.ts
@@ -1,3 +1,3 @@
-import { NodeCGBrowser, NodeCGStaticBrowser } from '../../../../../types/browser';
+import { NodeCGBrowser, NodeCGStaticBrowser } from 'nodecg/types/browser'; // eslint-disable-line
 
 export const { nodecg, NodeCG }: { nodecg: NodeCGBrowser, NodeCG: NodeCGStaticBrowser } = window;
