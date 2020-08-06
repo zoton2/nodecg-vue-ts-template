5e71b69c0a8d8a731dd394b23dc1c0a5a06b2201
zoton2 <zoton2@gmail.com>
2020-08-06 17:57:33 +0100

    More eslint rules/settings for types

--- .eslintrc.types.js
+++ .eslintrc.types.js
@@ -10,7 +10,14 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
   ],
+  settings: {
+    'import/extensions': ['.ts', '.tsx'],
+  },
   rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      ts: 'never',
+      tsx: 'never',
+    }],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true, // Some places have dev deps imported where eslint complains.
       packageDir: ['.', '../..'], // Check for deps in NodeCG folder as well.
@@ -18,5 +25,7 @@ module.exports = {
     'import/no-unresolved': [2, { caseSensitive: false }],
     'max-len': ['error', { 'code': 100 }],
     'lines-between-class-members': 'off',
+    'import/prefer-default-export': 'off',
+    'camelcase': 'off',
   },
 };
