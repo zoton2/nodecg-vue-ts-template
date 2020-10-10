5570194b48b4fb84aed2d9b00900b2a83fee489e
zoton2 <zoton2@gmail.com>
2020-10-10 17:19:58 +0100

    Fully make sure module-alias works, add some example code to check

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,7 +1,24 @@
+// This must go first so we can use module aliases!
+/* eslint-disable import/first */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('module-alias').addAlias('@', require('path').join(__dirname, '.'));
+
+import { Configschema } from '@/configschema';
+import { ExampleReplicant } from '@/schemas';
+import { ExampleType } from '@/types';
 import type { NodeCG } from 'nodecg/types/server';
 import { set } from './util/nodecg';
 
 export = (nodecg: NodeCG): void => {
   set(nodecg);
   nodecg.log.info('Extension code working!');
+
+  // Declaring a replicant.
+  const exampleRep = nodecg.Replicant<ExampleReplicant>('exampleReplicant');
+
+  // Access the bundle configuration with types.
+  const config = nodecg.bundleConfig as Configschema;
+
+  // Accessing normal types.
+  const exampleType: ExampleType = { exampleProperty: 'exampleString' };
 };
