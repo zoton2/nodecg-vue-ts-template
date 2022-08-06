8cb436cc05b7b77e82e840a11a4ca512d6555873
zoton2 <zoton2@gmail.com>
2022-08-06 23:01:33 +0100

    Add nodecg-vue-composable, add example code

--- src/dashboard/example/main.vue
+++ src/dashboard/example/main.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { ExampleType } from '@nodecg-vue-ts-template/types';
-import { Configschema } from '@nodecg-vue-ts-template/types/schemas';
+import { Configschema, ExampleReplicant } from '@nodecg-vue-ts-template/types/schemas';
 import { useHead } from '@vueuse/head';
+import { useReplicant } from 'nodecg-vue-composable';
 import { ref } from 'vue';
 
 const text = ref('Example');
 
 // Set the title of this page.
 useHead({ title: 'example' });
+
+// Helper composable to make accessing/modifying replicants easier.
+// For more information see https://github.com/Dan-Shields/nodecg-vue-composable
+const exampleReplicant = useReplicant<ExampleReplicant>(
+  'exampleReplicant',
+  'nodecg-vue-ts-template',
+);
 
 // Access the bundle configuration with types.
 const config = nodecg.bundleConfig as Configschema;
