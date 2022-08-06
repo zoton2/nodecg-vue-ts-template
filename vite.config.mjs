96ef5716f468f6b439860cea8361f7c863fe0289
zoton2 <zoton2@gmail.com>
2022-08-06 22:42:13 +0100

    Add env.d.ts file for Vue files

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,26 +6,24 @@ import checker from 'vite-plugin-checker';
 import NodeCGPlugin from 'vite-plugin-nodecg';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  const input = globbySync([
-    './src/dashboard/*.ts',
-    './src/graphics/*.ts',
-  ]);
-  return {
-    plugins: [
-      vue({
-        template: { transformAssetUrls },
-      }),
-      quasar({
-        autoImportComponentCase: 'pascal',
-      }),
-      checker({ vueTsc: { tsconfigPath: 'tsconfig.browser.json' } }),
-      NodeCGPlugin(),
-    ],
-    build: {
-      rollupOptions: {
-        input,
-      },
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      autoImportComponentCase: 'pascal',
+    }),
+    checker({ vueTsc: { tsconfigPath: 'tsconfig.browser.json' } }),
+    NodeCGPlugin(),
+  ],
+  build: {
+    rollupOptions: {
+      input: globbySync([
+        './src/dashboard/*.ts',
+        './src/graphics/*.ts',
+        '!**.d.ts',
+      ]),
     },
-  };
+  },
 });
