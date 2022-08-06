a228b51ca9d01f8b263bf354ef51c13358543009
zoton2 <zoton2@gmail.com>
2022-08-06 22:34:27 +0100

    Add some extra example code to dashboard/graphic

--- src/dashboard/example/main.vue
+++ src/dashboard/example/main.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { ExampleType } from '@nodecg-vue-ts-template/types';
+import { Configschema } from '@nodecg-vue-ts-template/types/schemas';
 import { useHead } from '@vueuse/head';
 import { ref } from 'vue';
 
+const text = ref('Example');
+
+// Set the title of this page.
 useHead({ title: 'example' });
 
-const text = ref('Example');
+// Access the bundle configuration with types.
+const config = nodecg.bundleConfig as Configschema;
+
+// Accessing normal types.
+const exampleType: ExampleType = { exampleProperty: 'exampleString' };
 </script>
 
 <template>
