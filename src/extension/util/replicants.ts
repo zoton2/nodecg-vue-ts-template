efaa38b8f27c88de6976acfa66ba4a1238b82dd8
zoton2 <zoton2@gmail.com>
2024-06-22 22:37:45 +0200

    Add replicant wrappers

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -2,14 +2,30 @@
 
 import type { ExampleReplicant } from '@nodecg-vue-ts-template/types/schemas';
 import type NodeCG from '@nodecg/types';
+import { join } from 'path';
 import { get as nodecg } from './nodecg';
+
+// Wrappers to help with the mess of typings with replicants.
+function wrapper<T>(name: string) {
+  // This path is relative to built files!
+  const path = join(__dirname, `../../schemas/${name}.json`);
+  return nodecg().Replicant<T>(name, { schemaPath: path }) as
+    unknown as T;
+}
+// Wrapper for replicants that have a default (based on schema).
+function hasDefault<T>(name: string) {
+  return wrapper<NodeCG.ServerReplicantWithSchemaDefault<T>>(name);
+}
+// Wrapper for replicants that don't have a default (based on schema).
+function hasNoDefault<T>(name: string) {
+  return wrapper<NodeCG.ServerReplicant<T>>(name);
+}
 
 /**
  * This is where you can declare all your replicant to import easily into other files,
  * and to make sure they have any correct settings on startup.
  */
-// TODO: Make notes about if you need "ServerReplicantWithSchemaDefault" or not.
 
 // YOU CAN REMOVE THIS RULE WHEN YOU GET MULTIPLE REPLICANTS!
 // eslint-disable-next-line import/prefer-default-export
-export const exampleReplicant = nodecg().Replicant<ExampleReplicant>('exampleReplicant') as unknown as NodeCG.ServerReplicantWithSchemaDefault<ExampleReplicant>;
+export const exampleReplicant = hasDefault<ExampleReplicant>('exampleReplicant');
