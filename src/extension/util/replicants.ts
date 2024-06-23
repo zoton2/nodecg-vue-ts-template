9a5447d3965c4bee4bfbcbcf3264c35327f13089
zoton2 <zoton2@gmail.com>
2024-06-23 16:46:19 +0200

    Remove main replicant wrapper and schemaPath
    schemaPath is only needed if your bundle is not in the normal `bundles` folder.

--- src/extension/util/replicants.ts
+++ src/extension/util/replicants.ts
@@ -2,23 +2,15 @@
 
 import type { ExampleReplicant } from '@nodecg-vue-ts-template/types/schemas';
 import type NodeCG from '@nodecg/types';
-import { join } from 'path';
 import { get as nodecg } from './nodecg';
 
-// Wrappers to help with the mess of typings with replicants.
-function wrapper<T>(name: string) {
-  // This path is relative to built files!
-  const path = join(__dirname, `../../schemas/${name}.json`);
-  return nodecg().Replicant<T>(name, { schemaPath: path }) as
-    unknown as T;
-}
 // Wrapper for replicants that have a default (based on schema).
 function hasDefault<T>(name: string) {
-  return wrapper<NodeCG.ServerReplicantWithSchemaDefault<T>>(name);
+  return nodecg().Replicant<T>(name) as unknown as NodeCG.ServerReplicantWithSchemaDefault<T>;
 }
 // Wrapper for replicants that don't have a default (based on schema).
 function hasNoDefault<T>(name: string) {
-  return wrapper<NodeCG.ServerReplicant<T>>(name);
+  return nodecg().Replicant<T>(name) as unknown as NodeCG.ServerReplicant<T>;
 }
 
 /**
