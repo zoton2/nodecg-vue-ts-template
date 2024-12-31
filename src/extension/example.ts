eb4ee5e37b6647f473cc808d8a72b7acf85e3b8b
zoton2 <zoton2@gmail.com>
2024-12-31 15:16:10 +0000

    more example code in extension

--- src/extension/example.ts
+++ src/extension/example.ts
@@ -2,11 +2,14 @@ import type { ExampleType } from '../types/index.js';
 import { nodecg } from './util/nodecg.js';
 import { exampleReplicant } from './util/replicants.js';
 
-
+// Example code:
+// Log to show things are working.
 nodecg.log.info('Extension code working!');
+// Access this bundle's configuration with no type assertion needed.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const configProperty = nodecg.bundleConfig.exampleProperty;
 // Access/set a replicant (also see ./util/replicants).
 exampleReplicant.value = { exampleProperty: `exampleString_Changed_${Date.now()}` };
-
 // Accessing normal types.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const exampleType: ExampleType = { exampleProperty: 'exampleString' };
