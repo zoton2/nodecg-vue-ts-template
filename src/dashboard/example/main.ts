78b38da7117ce63805f2f09d3142df0d180f44b4
zoton2 <zoton2@gmail.com>
2025-03-16 04:00:43 +0000

    remove import of quasar.css
    I don't think this is needed anymore according to the config page if you also import the sass file

--- src/dashboard/example/main.ts
+++ src/dashboard/example/main.ts
@@ -2,7 +2,6 @@ import '@quasar/extras/material-icons/material-icons.css';
 import '@quasar/extras/roboto-font/roboto-font.css';
 import { createHead } from '@unhead/vue';
 import { Dark, Quasar } from 'quasar';
-import 'quasar/dist/quasar.css';
 import 'quasar/src/css/index.sass';
 import { createApp } from 'vue';
 import App from './main.vue';
